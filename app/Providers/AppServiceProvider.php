4477ae5b28aed3d9b1cc60bd9ad5a1f938dfe6b3
shohss <rakkidaisuki4649@yahoo.co.jp>
2022-12-02 19:31:57 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        if(\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
