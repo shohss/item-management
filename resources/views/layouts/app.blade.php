199ddecfc3ea8e991f9261ab7151f67f0f7902db
shohss <rakkidaisuki4649@yahoo.co.jp>
2023-02-07 12:24:27 +0900

    ログイン画面の日本語対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a>
                                 </li>
                             @endif
                         @else
